71bac123641af3dc6c143eb805a4db1a7ff07f5b
Ankala pavan sai teja <pavanmacbook@Ankalas-MacBook-Air.local>
2025-06-29 11:35:40 +0530

    Add whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,35 @@
+#Example 1: Reverse countdown
+i=int(input("Enter a value:"))
+while i>=1:
+    print("Countdown:",i)
+    i=i+1
+
+#Example 2: Sum of first N numbers
+n=int(input("Enter a value:"))
+i=1
+total=0
+while i<=n:
+    total+=i
+    i+=1
+
+#Example 3:Print all Even numbers from 1 to N
+i=1
+n=int(input("Enter a value:"))
+while i<=n:
+    if i%2==0:
+        print(i)
+    i+=1
+
+#Example 4:Infinite Loop
+While True:
+    name=input("Type 'exit' to stop:")
+    if name=="exit":
+       break
+    print("You typed ",name)
+
+#Example 5:Prints each value of a string
+text=input("Enter a name:")
+i=0
+while i<len(text):
+    print(text[i])
+    i+=1
