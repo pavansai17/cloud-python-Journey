40b2f38b6ff5ac802e41b87dbb07a34d05c70019
pavansai17 <127409710+pavansai17@users.noreply.github.com>
2025-07-02 11:58:29 +0530

    Update whileloop and forloop.py

--- whileloop.py
+++ whileloop.py
@@ -1,8 +1,8 @@
 #Example 1: Reverse countdown
-i=int(input("Enter a value:"))
-while i>=1:
-    print("Countdown:",i)
-    i=i+1
+i = 5
+while i >= 1:
+    print("Countdown:", i)
+    i -= 1
 
 #Example 2: Sum of first N numbers
 n=int(input("Enter a value:"))
@@ -11,7 +11,7 @@ total=0
 while i<=n:
     total+=i
     i+=1
-
+print("Total sum is ",total)
 #Example 3:Print all Even numbers from 1 to N
 i=1
 n=int(input("Enter a value:"))
@@ -21,11 +21,12 @@ while i<=n:
     i+=1
 
 #Example 4:Infinite Loop
-While True:
-    name=input("Type 'exit' to stop:")
-    if name=="exit":
-       break
-    print("You typed ",name)
+while True:
+    name = input("Type 'exit' to stop: ")
+    if name == "exit":
+        break
+    print("You typed:", name)
+
 
 #Example 5:Prints each value of a string
 text=input("Enter a name:")
