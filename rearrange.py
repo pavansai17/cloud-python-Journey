269f12f48a9682013db315f8415bab85390e4723
pavansai17 <127409710+pavansai17@users.noreply.github.com>
2026-03-14 21:01:02 +0530

    Arranging values of array in ascend and descend order

--- rearrange.py
+++ rearrange.py
@@ -10,3 +10,7 @@ def rearrange(arr):
     return result
 arr = [2,5,11,5,62,1,5]
 print(rearrange(arr))
+# Here three things are happening 
+# 1.Sort the array
+# 2.Entire array is split into two components
+# 3.Reverse the second half of the array.
