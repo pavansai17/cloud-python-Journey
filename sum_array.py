d2ea3cce48b16d0531bdd59039d0d74dcf898f15
pavansai17 <127409710+pavansai17@users.noreply.github.com>
2026-03-14 21:31:57 +0530

    Total sum of an array is displayed

---
+++ sum_array.py
@@ -0,0 +1,8 @@
+# Start sum is zero
+# Traverse each element of the array
+# Add each element sum.
+arr = [2,5,1,3,0]
+total = 0
+for num in arr:
+    total = total + num
+print("Total sum is:",total)
