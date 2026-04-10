e4fd6effb936e7d3cbea76d60c212a2602c76a41
pavansai17 <127409710+pavansai17@users.noreply.github.com>
2026-04-10 19:58:18 +0530

    Introduction to python

---
+++ intro.py
@@ -0,0 +1,10 @@
+"""
+1. Introduction to Python
+1.1 What is Python?
+1.2 History and Evolution of Python
+1.3 Features and Advantages of Python
+1.4 Real-World Applications of Python
+1.5 Installing Python and Setting Up the Environment
+1.6 Running Python Scripts (Interactive Mode vs. Script Mode)
+1.7 Understanding Python’s REPL (Read-Eval-Print Loop)
+"""
